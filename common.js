5ea7db7cd763e8a4e507c180cf4fc9a65a48d898
Ziv <ziv.israeli01@gmail.com>
2019-11-02 09:28:21 -0700

    mistake

--- common.js
+++ common.js
@@ -5,8 +5,10 @@
 getCookie = (name) => {
   // since document.cookie returns all cookie, match would filter out the one I need.
   // the match regex: cookie name should follow an equal sign AND NOT a space of a semi-colon.
+  // with 'capturing groups' we get an array with the array[0] is the entire expression value.
+  // the rest of the values are the group's value.
   let value = document.cookie.match('[; ]'+ name + '=([^\\s;]*)');
-  return value ? value[2] : null;
+  return value ? value[1] : null;
 }
 
 setCookie = (name, value, days = 365) => {
